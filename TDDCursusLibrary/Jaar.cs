6334877a309165a79dafa58ab470292ab13c2f8b
Michael <Michaelbracke@hotmail.com>
2019-11-04 10:49:35 +0100

    Initialisatie test project

---
+++ TDDCursusLibrary/Jaar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDCursusLibrary
+{
+    class Jaar
+    {
+
+        private readonly int jaar;
+
+        public Jaar (int jaar)
+        {
+            this.jaar = jaar;
+        }
+
+        public bool isSchrikkeljaar
+        {
+            get
+            {
+                if ( jaar % 400 == 0)
+                {
+                    return true;
+                }
+
+                if ( jaar % 100 == 0)
+                {
+                    return false;
+                }
+
+                return jaar % 4 == 0;
+            }
+        }
+            
+    }
+}
---
+++ TDDcursusLibraryTEST/JaarTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDcursusLibraryTEST
+
+namespace TDDcursusLibraryTEST
+{
+    [TestClass]
+
+    public class JaarTest
+
+    {
+        [TestMethod]
+        public void EenJaarDeelbaarDoor400IsEenSchrikkeljaar()
+        {
+            Assert.are
+        }
+    }
+}
